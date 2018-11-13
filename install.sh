868e8f067ab0c08427ea27a23a5e7a768988ce6b
chrisr3d <chris.studer.68@gmail.com>
2018-11-13 11:44:06 +0100

    add: Added installation of some vim configuration files

--- install.sh
+++ install.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+#Installing some usefull packages
+sudo apt-get install curl vim-gnome screen git tig
+
+#Installing Pathogen
+curl -Lls https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > _vim/autoload/pathogen.vim
+
+#Installing airline
+dir=_vim/bundle/vim-airline
+if [ ! -d "$dir" ]; then
+    git clone https://github.com/vim-airline/vim-airline $dir
+fi
+
+#Installing airline themes
+dir=$dir'-themes'
+if [ ! -d "$dir" ]; then
+    git clone https://github.com/vim-airline/vim-airline-themes $dir
+fi
+
+#Installing powerline fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts/
+./install.sh
+cd ..
+rm -r fonts
+
+#Installing vim-gitgutter
+dir=_vim/bundle/vim-gitgutter
+if [ ! -d "$dir" ]; then
+    git clone https://github.com/airblade/vim-gitgutter.git $dir
+fi
+
+#Installing vim-indentLine
+dir=_vim/bundle/indentLine
+if [ ! -d "$dir" ]; then
+    git clone https://github.com/Yggdroot/indentLine.git $dir
+fi
+
+
 set -e
 
 function link_file {
