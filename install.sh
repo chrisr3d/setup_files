d0119d20c553ec448230e4c43a584d7f771015f6
Christian Studer <christian.studer@circl.lu>
2024-01-03 16:17:43 +0100

    chg: Updated setup files for a MacOS bash environment

--- install.sh
+++ install.sh
@@ -1,51 +1,10 @@
 #!/usr/bin/env bash
 
-#Installing some usefull packages
-sudo apt-get install curl vim-gnome screen git tig python3-pip -y
-pip3 install pew
+cd "$(dirname "${BASH_SOURCE}")";
 
-#Installing Pathogen
-dir=_vim/autoload
-if [ ! -d "$dir" ]; then
-    mkdir $dir
-fi
-curl -LSso _vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git pull origin main;
 
-#Installing airline
-dir=_vim/bundle/vim-airline
-if [ ! -d "$dir" ]; then
-    git clone https://github.com/vim-airline/vim-airline $dir
-fi
-
-#Installing airline themes
-dir=$dir'-themes'
-if [ ! -d "$dir" ]; then
-    git clone https://github.com/vim-airline/vim-airline-themes $dir
-fi
-
-#Installing powerline fonts
-git clone https://github.com/powerline/fonts.git
-cd fonts/
-./install.sh
-cd ..
-sudo rm -r fonts
-
-#Installing vim-gitgutter
-dir=_vim/bundle/vim-gitgutter
-if [ ! -d "$dir" ]; then
-    git clone https://github.com/airblade/vim-gitgutter.git $dir
-fi
-
-#Installing vim-indentLine
-dir=_vim/bundle/indentLine
-if [ ! -d "$dir" ]; then
-    git clone https://github.com/Yggdroot/indentLine.git $dir
-fi
-
-
-set -e
-
-function link_file {
+function link_file() {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
 
@@ -56,14 +15,21 @@ function link_file {
     ln -sf ${source} ${target}
 }
 
-if [ "$1" = "vim" ]; then
-    for i in _vim*
-    do
-       link_file $i
-    done
-else
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
     for i in _*
     do
         link_file $i
     done
-fi
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        for i in _*
+        do
+            link_file $i
+        done
+    fi;
+fi;
+unset link_file;
+
+source ~/.bash_profile;
